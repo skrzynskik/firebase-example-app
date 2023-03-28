3199ab488d63d0bc6b45ef73e81078fc9a08e1ac
skrzynski.k <kacperskrzynski@outlook.com>
2023-03-28 14:04:56 +0200

    initial commit

---
+++ databaseget.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTPJNB8d8MhTl30LcMs6eVA9Gd5octO4Q",
+  authDomain: "uek-mobile.firebaseapp.com",
+  projectId: "uek-mobile",
+  storageBucket: "uek-mobile.appspot.com",
+  messagingSenderId: "738793447529",
+  appId: "1:738793447529:web:12ab788620d7cee6d502f8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+app.
